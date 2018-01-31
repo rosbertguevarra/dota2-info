aee868c99733f28cf726e83f9b48069a4fce4d97
Rosbert Guevarra <rosbert01@yahoo.com>
2018-01-31 17:52:14 +0800

    added search mmr for live games

--- src/LiveGames.js
+++ src/LiveGames.js
@@ -9,20 +9,34 @@ const alignGrid = {
     flexWrap: "wrap"
 }
 
+const alignSearch = {
+    margin: "0px 0px 0px 200px",
+    padding: "50px"
+}
+
+const styleInput ={
+    padding: "10px 200px 10px 10px"
+}
+
 const URL = "https://api.opendota.com/api/live";
 
 
 axios.get(URL)
-  .then(function (response) {
-    console.log(response);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+    .then(function (response) {
+        console.log(response);
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
 
 class LiveGames extends Component {
     state = {
-        data: []
+        data: [],
+        searchTerm: ""
+    }
+
+    handleSearchTerm = (event) => {
+        this.setState({searchTerm: event.target.value});
     }
 
     componentDidMount() {
@@ -34,22 +48,34 @@ class LiveGames extends Component {
             });
     }
     render() {
-        const Live = this.state.data.map(live => (
+        const mmr = 5400;
+
+        const Filter = this.state.data.filter(stat =>`${stat.average_mmr}`.toUpperCase().
+        indexOf(this.state.searchTerm.toUpperCase()) >= 0)
+        .map(live => 
             <Games
 
                 league_id={live.league_id}
-                activate_time = {live.activate_time}
-                average_mmr = {live.average_mmr}
-                radiant_score = {live.radiant_score}
-                dire_score = {live.dire_score}
-                spectators = {live.spectators}
+                activate_time={live.activate_time}
+                average_mmr={live.average_mmr}
+                radiant_score={live.radiant_score}
+                dire_score={live.dire_score}
+                spectators={live.spectators}
             />
 
-        ))
+        )
         return (
             <div>
                 <Sidebar />
-            <div style = {alignGrid}>{Live}</div>
+                <div style={alignSearch}>
+                    Search:  <input style={styleInput}
+                        onChange={this.handleSearchTerm}
+                        value={this.state.searchTerm}
+                        type="text"
+                        placeholder="Search mmr"
+                    />
+                </div>
+                <div style={alignGrid}>{Filter}</div>
             </div>
         )
     }
